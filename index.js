6685d9d7902ade642161d7076d7322f9197fdb94
Miquella <jaypeejavierash@gmail.com>
2025-09-03 22:51:36 +0800

    HEHE ADDED BETTER COMMENTS AND ADD AND REMOVE FUNCTIONALITY

--- index.js
+++ index.js
@@ -12,13 +12,39 @@ document.querySelector(".task").addEventListener("keydown", function(event){
 
 function todo () {
     let box = document.querySelector(".task");
-    let li = document.createElement("li");//creates an element whenever the button is pressed
+    let li = document.createElement("li");//creates a list eleent whenever the button is pressed
     let input = document.querySelector(".task").value; //gets the value of the text input
+    if (input == "") {
+        alert ("please input a Text")
+    }
     li.textContent = input;//gets the input of the text input into the li
     document.querySelector(".lists").appendChild(li) //then append it so that the list gets visible
     box.value = "";//clears the text box after inputting a task
     let remove = document.createElement("button"); //creates a remove button
     remove.textContent = "❌"
     remove.classList.add("remove-btn")
-    li.appendChild(remove)
+    let add = document.createElement("button")
+    add.textContent = "✔️"
+    add.classList.add("add-btn")
+    let btnGroup = document.createElement("div")
+    btnGroup.classList.add("btn-container")
+    btnGroup.appendChild(add)
+    btnGroup.appendChild(remove)
+    li.appendChild(btnGroup)
+    let buton = document.querySelectorAll(".btn-container").length
+    for (let i = 0; i< buton; i++){
+        for (j = 0; j <buton; j++) {
+    document.querySelectorAll(".remove-btn")[i].addEventListener("click", function(){
+        this.parentElement.parentElement.remove(remove)
+
+    })
+
+    document.querySelectorAll(".add-btn")[j].addEventListener("click", function() {
+             this.parentElement.parentElement.classList.toggle("done")
+             setTimeout(() => {
+                this.parentElement.parentElement.remove(add)
+             },3000)
+    })
+}
+}
 }
